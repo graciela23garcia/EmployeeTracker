1f799a05b251f75730c62b89b14de443e5d33727
graciela23garcia <70188756+graciela23garcia@users.noreply.github.com>
2021-02-24 22:09:41 -0800

    update role of employee function created

--- server.js
+++ server.js
@@ -98,3 +98,48 @@ function viewRoles() {
       prompts();
     });
   }
+
+  
+function updateEmployeeRole() {
+    connection.query(
+      "SELECT role.id, title, first_name, last_name, employee.id, employee.role_id FROM role LEFT JOIN employee on role.id = employee.role_id;",
+      (err, res) => {
+        if (err) throw err;
+        let employee_ids = res.map(({ id, first_name, last_name }) => ({
+          name: first_name + " " + last_name,
+          value: id,
+        }));
+        let role_ids = res.map(({ role_id, title }) => ({
+          name: title,
+          value: role_id,
+        }));
+        console.table(res);
+        inquirer
+          .prompt([
+            {
+              type: "list",
+              name: "employee_id",
+              message: "What is the employee's name?",
+              choices: employee_ids,
+            },
+            {
+              type: "list",
+              name: "newrole_id",
+              message: "What is the new role title?",
+              choices: role_ids,
+            },
+          ])
+          .then((data) => {
+            connection.query(
+              "UPDATE employee SET ? WHERE ?",
+              [{ role_id: data.newrole_id }, { id: data.employee_id }],
+              (err, res) => {
+                if (err) throw err;
+                console.log(res.affectedRows + " has been updated!\n");
+                prompts();
+              }
+            );
+          });
+      }
+    );
+  }

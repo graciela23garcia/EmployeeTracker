44255e6da52d1ff61f9401ef57ca3a55ad59f391
graciela23garcia <70188756+graciela23garcia@users.noreply.github.com>
2021-02-24 22:46:35 -0800

    added dependancies

---
+++ seed.sql
@@ -0,0 +1,15 @@
+USE employeeInfo_db;
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Mark", "Johnnson", 3, 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Macie", "Sean", 2, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Martha", "Lauren", 1, 2);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Barista", 10000, 1)
+INSERT INTO role (title, salary, department_id)
+VALUES ("Coffee Stand Manager", 20000, 1)
+INSERT INTO role (title, salary, department_id)
+VALUES ("Barista2", 30000, 1)
+INSERT INTO department (name)
+VALUES ("Coffee Stand")

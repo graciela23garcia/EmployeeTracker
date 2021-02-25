7437723e8ff23fe42a7666106a332ea9a3342307
graciela23garcia <70188756+graciela23garcia@users.noreply.github.com>
2021-02-24 21:46:21 -0800

    created connection to database

--- server.js
+++ server.js
@@ -1,2 +1,17 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+require("console.table");
+
+
+//creating connection to the database
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "elemelon23",
+    database: "employeesDB"
+});
+connection.connect(function (err) {
+    if (err) throw err;
+    runSearch();
+});

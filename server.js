27c0ef64971e1dfeeb10a497091b30cc2828cb7f
graciela23garcia <70188756+graciela23garcia@users.noreply.github.com>
2021-02-24 22:01:18 -0800

    created array of prompts

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+const { prompts } = require("inquirer");
 require("console.table");
 
 
@@ -13,5 +14,64 @@ const connection = mysql.createConnection({
 });
 connection.connect(function (err) {
     if (err) throw err;
-    runSearch();
+    prompts();
 });
+
+function prompts() {
+    inquirer
+        .prompt({
+                type: "list",
+                name: "action",
+                message: "What would you like to do?",
+                choices: [
+                    "view all employees",
+                    "add employee",
+                    "view all employees by department",
+                    "view all departments",
+                    "add department",
+                    "view all roles",
+                    "update employee role",
+                    "add role",
+                    "exit"
+                ]
+            })
+            .then(function (answer) {
+            switch (answer.action) {
+                case "view all employees":
+                    employeeView();
+                    break;
+
+                case "add employee":
+                    addEmployee();
+                    break;
+
+                case "view all employees by department":
+                    employeesByDepartment();
+                    break;
+
+                case "view all departments":
+                    viewDepartments();
+                    break;
+
+                case "add department":
+                    addDepartment();
+                    break;
+
+                case "view all roles":
+                    viewRoles();
+                    break;
+
+                case "update employee role":
+                    updateEmployeeRole();
+                    break;
+
+                case "add role":
+                    addRole();
+                    break;
+
+                case "exit":
+                    connection.end();
+                    break;
+            }
+        });
+}
